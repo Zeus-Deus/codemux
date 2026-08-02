f1449a03a6e062e99717ab05b5df8065fdd68afe
Zeus-Deus <github.commits@widow.cc>
2026-08-02 14:17:56 +0200

    ci: activate Windows test manifests before execution

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,7 +1,3 @@
 fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
-        println!("cargo:rustc-link-arg-tests=/DELAYLOAD:comctl32.dll");
-        println!("cargo:rustc-link-arg-tests=delayimp.lib");
-    }
     tauri_build::build()
 }
