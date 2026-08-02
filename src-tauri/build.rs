f4a4a73e0d442a4c8f0cfa0f374188760d3ba539
Zeus-Deus <github.commits@widow.cc>
2026-08-02 13:22:21 +0200

    fix: use response file for Windows test manifest

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,15 @@
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
-        println!("cargo:rustc-link-arg-tests=/MANIFEST:EMBED");
-        println!(
-            r#"cargo:rustc-link-arg-tests="/MANIFESTDEPENDENCY:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'""#
-        );
+        let response_file = std::path::Path::new(&std::env::var("OUT_DIR").unwrap())
+            .join("test-common-controls-v6.rsp");
+        std::fs::write(
+            &response_file,
+            r#"/MANIFEST:EMBED
+/MANIFESTDEPENDENCY:"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'"
+"#,
+        )
+        .expect("failed to write Windows test linker response file");
+        println!("cargo:rustc-link-arg-tests=@{}", response_file.display());
     }
     tauri_build::build()
 }
